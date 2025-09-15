344b4b3c973623a9af7359345b13be8ab6fd35c8
Zephyr_Riofrio <exodusgarciariofrio@gmail.com>
2025-09-14 22:17:27 -0500

    Actualizando el código js

--- js/voice-search.js
+++ js/voice-search.js
@@ -1,3 +1,6 @@
+document.querySelector('.form__microphone-icon').addEventListener("click",voiceSearchModalOpen);
+document.querySelector(".voice-search__close-modal").addEventListener("click",voiceSearchModalClose);
+document.querySelector(".voice-search__microphone-border").addEventListener("click",voiceRecognition)
 
 const voiceSearch = document.querySelector(".voice-search");
 let microAceptado = false;
@@ -36,7 +39,3 @@ const voiceRecognition = () =>{
 }
     recognition.start();
 }
-
-document.querySelector('.form__microphone-icon').addEventListener("click",voiceSearchModalOpen);
-document.querySelector(".voice-search__close-modal").addEventListener("click",voiceSearchModalClose);
-document.querySelector(".voice-search__microphone-border").addEventListener("click",voiceRecognition)
